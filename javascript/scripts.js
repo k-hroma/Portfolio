651ddd333c8581c12282107fa041b82d94c65732
k-hroma <rociomendonca@gmail.com>
2025-01-23 18:08:06 -0300

    JavaScript added

---
+++ javascript/scripts.js
@@ -0,0 +1,7 @@
+function changeText() {
+  document.getElementById("scroll-change").innerHTML = "*down";
+}
+
+function changeTextS() {
+  document.getElementById("scroll-change").innerHTML = "*scroll";
+}
