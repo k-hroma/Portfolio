f8c7c5ddba46296a3f8558ed3175f21dd13eeef6
k-hroma <rociomendonca@gmail.com>
2025-01-30 14:40:28 -0300

    javascriptfunctions added

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,5 @@
+// Scroll Change Word
+
 function changeText() {
   document.getElementById("scroll-change").innerHTML = "*⎚-⎚";
 }
@@ -5,3 +7,33 @@ function changeText() {
 function changeTextS() {
   document.getElementById("scroll-change").innerHTML = "*scroll";
 }
+
+// Form
+// Select the submit button
+document
+  .getElementById("btn-submit")
+  .addEventListener("click", function (event) {
+    // Prevent default form submission behavior
+    event.preventDefault();
+
+    // Get values from the form inputs
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const telephone = document.getElementById("telephone").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Store data in variables
+    console.log("Name:", name);
+    console.log("Surname:", surname);
+    console.log("Telephone:", telephone);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    // Clear the form fields (this will make placeholders visible again)
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("telephone").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  });
