2efda1b5be0b965292e5203d6ae2c0cdc7f3ba3c
k-hroma <rociomendonca@gmail.com>
2025-02-03 14:13:04 -0300

    javascript added to form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,12 +10,14 @@ function changeTextS() {
 
 // Form
 // Select the submit button
+contador = 0;
 document
   .getElementById("btn-submit")
   .addEventListener("click", function (event) {
     // Prevent default form submission behavior
     event.preventDefault();
 
+    const id = (contador += 1);
     // Get values from the form inputs
     const name = document.getElementById("name").value;
     const surname = document.getElementById("surname").value;
@@ -24,11 +26,32 @@ document
     const message = document.getElementById("message").value;
 
     // Store data in variables
+    console.log("Id:", id);
     console.log("Name:", name);
     console.log("Surname:", surname);
     console.log("Telephone:", telephone);
     console.log("Email:", email);
     console.log("Message:", message);
+
+    // hago un array con esos valores
+
+    const mailInfo = {
+      Id: id,
+      Name: name,
+      Surname: surname,
+      Telephone: telephone,
+      Email: email,
+      Message: message,
+    };
+    console.log(mailInfo);
+    console.log(typeof mailInfo);
+
+    //convierto array a json
+    const jsonInfo = JSON.stringify(mailInfo);
+    console.log(jsonInfo);
+
+    //guardo la info en json
+    localStorage.setItem("InfoMail", jsonInfo);
 
     // Clear the form fields (this will make placeholders visible again)
     document.getElementById("name").value = "";
